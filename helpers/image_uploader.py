fb53da04651852abe4352ab5d4f60f4e7299e637
qaqaqaqaowh <nicholasowh@hotmail.com>
2018-11-28 14:47:11 +0800

    fix

--- helpers/image_uploader.py
+++ helpers/image_uploader.py
@@ -9,11 +9,11 @@ def upload_image(img):
     if not os.environ.get("FLASK_ENV"):
         from google.oauth2 import service_account
         gcs._credentials = service_account.Credentials.from_service_account_info(
-            json.load(io.StringIO(os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")))))
-    bucket=gcs.get_bucket(os.environ.get("GCS_BUCKET"))
-    blob=bucket.blob(img.filename)
+            json.load(io.StringIO(os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))))
+    bucket = gcs.get_bucket(os.environ.get("GCS_BUCKET"))
+    blob = bucket.blob(img.filename)
     blob.upload_from_string(
         img.read(),
-        content_type = img.content_type
+        content_type=img.content_type
     )
     return blob.public_url
