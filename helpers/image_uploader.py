2c762a7e763cb02dd5d368c7e1a1b69c560a9480
qaqaqaqaowh <nicholasowh@hotmail.com>
2018-11-28 12:12:45 +0800

    test for image upload

--- helpers/image_uploader.py
+++ helpers/image_uploader.py
@@ -1,9 +1,17 @@
 from google.cloud import storage
 import os
+import io
+import json
 
 
 def upload_image(img):
-    gcs = storage.Client()
+    gcs = None
+    if os.environ.get("FLASK_ENV"):
+        gcs = storage.Client()
+    else:
+        from google.oauth2 import service_account
+        gcs = storage.Client(credetials=service_account.Credentials.from_service_account_info(
+            json.load(io.StringIO('{"asd": 123}'))))
     bucket = gcs.get_bucket(os.environ.get("GCS_BUCKET"))
     blob = bucket.blob(img.filename)
     blob.upload_from_string(
